b2774f1a083e9a33e4f304eeb37b95d44bf1af7e
Sowndhar <sowndharnewbie@gmail.com>
2023-07-23 10:10:11 +0530

    First commit

---
+++ src/main/java/com/system/TourBookingSystem/TourBookingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.system.TourBookingSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourBookingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TourBookingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/system/TourBookingSystem/TourBookingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.system.TourBookingSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TourBookingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
